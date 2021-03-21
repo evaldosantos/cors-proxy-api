37fe2de1fd1a61be57c78e3935d9947bf2e9f3d6
Evaldo Santos <jesantosjr@protonmail.com>
2021-03-21 18:45:17 -0300

    simple cors proxy

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const BASE_URL = process.env.BASE_URL;
+const PROXY_URL = process.env.PROXY_URL;
+
+const parse = (response) => {
+  let { next, previous } = response;
+
+  if (next && next.includes(PROXY_URL)) {
+    next = next.replace(PROXY_URL, BASE_URL);
+  }
+
+  if (previous && previous.includes(PROXY_URL)) {
+    previous = previous.replace(PROXY_URL, BASE_URL);
+  }
+
+  return {
+    ...response,
+    next,
+    previous,
+  };
+};
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.all("*", (req, res) => {
+  const { method, body } = req;
+  let config = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  if (method === "POST") {
+    config = { ...config, body: JSON.stringify(body) };
+  }
+
+  fetch(`${PROXY_URL}${req.originalUrl}/`, config)
+    .then((response) => {
+      if (response.ok) {
+        return response;
+      }
+      throw "Response Error?";
+    })
+    .then((response) => {
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.indexOf("application/json") !== -1) {
+        return response.json();
+      }
+
+      throw "Content type error?";
+    })
+    .then((response) => {
+      console.log(response);
+      return res.json(parse(response));
+    })
+    .catch((err) => {
+      return res.status(500).json({ type: "error", message: err.message });
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
